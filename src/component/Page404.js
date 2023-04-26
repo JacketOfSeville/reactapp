9f1d3277681cfd035229b6ac72a528a9904790da
Jacket <jacketfollowme@gmail.com>
2023-04-26 11:32:10 -0300

    Melhora do Fundo e Imagem Local

--- src/component/Page404.js
+++ src/component/Page404.js
@@ -1,10 +1,11 @@
 import styles from './css/404.module.css'
 import { Link } from 'react-router-dom';
+import Gato404 from './img/404.jpg';
 
 function Page404() {
     return (
         <div>
-            <img className={styles.center} src='https://http.cat/404.jpg'/>
+            <img alt='' className={styles.center} src={Gato404}/>
             <h1 className={styles.text}>
                 <Link className={styles.text} to='/'>Voltar ao início</Link>
             </h1>
